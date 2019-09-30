ee8f702b7ea5fa7eab2644aa53b768f58da536c3
Chris Chitla <chrischitla@gmail.com>
2019-09-29 20:37:05 -0700

    added function to calculate time of next arrival based on first train time and frequency inputs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,47 +1,84 @@
 $(document).ready(function () {
 
-    console.log("test log");
-    
     // Your web app's Firebase configuration
-    // var firebaseConfig = {
-    //     apiKey: "AIzaSyDOT7mATLVmlyeN7PauFXyLg3ipA70mq7w",
-    //     authDomain: "train-scheduler-8349f.firebaseapp.com",
-    //     databaseURL: "https://train-scheduler-8349f.firebaseio.com",
-    //     projectId: "train-scheduler-8349f",
-    //     storageBucket: "",
-    //     messagingSenderId: "875390957399",
-    //     appId: "1:875390957399:web:eee467e87acc4bdb2a3012"
-    // };
+    var firebaseConfig = {
+        apiKey: "AIzaSyDOT7mATLVmlyeN7PauFXyLg3ipA70mq7w",
+        authDomain: "train-scheduler-8349f.firebaseapp.com",
+        databaseURL: "https://train-scheduler-8349f.firebaseio.com",
+        projectId: "train-scheduler-8349f",
+        storageBucket: "",
+        messagingSenderId: "875390957399",
+        appId: "1:875390957399:web:eee467e87acc4bdb2a3012"
+    };
     // Initialize Firebase
-    // firebase.initializeApp(firebaseConfig);
+    firebase.initializeApp(firebaseConfig);
 
-    // let db = firebase.database();
-
-
+    let db = firebase.database();
 
 
-    $("#submit-btn").on("click", function(){
+
+
+
+
+
+
+    $("#submit-btn").on("click", function () {
         let trainName = $("#train-name").val();
         let destination = $("#destination").val();
         let firstTrainTime = $("#first-train-time").val();
         let frequency = $("#frequency").val();
-        populateList(trainName, destination, frequency);
+        populateList(trainName, destination, frequency, firstTrainTime);
     })
 
 
-    function populateList (trainName, destination, frequency) {
+
+
+
+
+
+
+
+    function populateList(trainName, destination, frequency, firstTrainTime) {
+        let nextArrival = nextArrivalTime(firstTrainTime, frequency)
+        //calculate next arrival time
+        //calculate minutes away
+
         let newTrain = $("<tr>");
         newTrain.html(`
             <td>${trainName}</td>
             <td>${destination}</td>
-            <td>${frequency}</td>`
-            // <td>${nextArrival}</td>
+            <td>${frequency}</td>
+            <td>${nextArrival}</td>`
             // <td>${minutesAway}</td>
-            );
+        );
 
         $("#train-list").append(newTrain);
     };
-   
-    
+
+
+
+    function nextArrivalTime(firstTrainTime, frequency) {
+        let currentHour = moment().hour();
+        let currentMinute = moment().minute();
+        let totalMinsNow = currentMinute + (currentHour * 60);
+
+        let inputTime = firstTrainTime.split(":");
+        let minsOfFirstTrain = parseInt(inputTime[0]) * 60 + parseInt(inputTime[1]);
+
+        let arrivalTimeCounter = minsOfFirstTrain;
+        let freq = parseInt(frequency);
+
+        while (arrivalTimeCounter < totalMinsNow) {
+            arrivalTimeCounter = arrivalTimeCounter + freq;
+        };
+        
+        let arrivalTimeStr = arrivalTimeCounter.toString();
+
+        let nextTrain = `${arrivalTimeStr[0]}${arrivalTimeStr[1]}:${arrivalTimeStr[2]}${arrivalTimeStr[3]}`
+
+        return nextTrain;
+    };   
+
+
 
 });
